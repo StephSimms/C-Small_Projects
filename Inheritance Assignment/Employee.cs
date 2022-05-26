7b9e0d38c7159471d491051b40941b47376a86cd
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-05-26 13:45:58 -0300

    Add project files.

---
+++ Inheritance Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_Assignment
+{
+    class Employee : Person
+    {
+        public int ID { get; set; }
+        public void SayName(string FirstName, string LastName)
+        {
+            Console.WriteLine(FirstName + "" + LastName);
+        }
+    }
+}
---
+++ Inheritance Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.FirstName = "Test";
+            person.LastName = "Testing";
+
+            person.SayName(person.FirstName, person.LastName);
+
+            Console.ReadLine();
+        }
+    }
+}
